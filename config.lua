c9309ae45538db5f44fbbc1e320163eed0965c68
Guardian Labs <austingbball3@icloud.com>
2025-03-15 22:27:29 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,3 @@
+Config = {}
+
+Config.Webhook = "YOUR_DISCORD_WEBHOOK_URL"  -- Set your Discord webhook URL here
---
+++ config/config.lua
@@ -0,0 +1,3 @@
+Config = {}
+
+Config.Webhook = "YOUR-DISCORD-WEBHOOK"  -- Set your Discord webhook URL here
